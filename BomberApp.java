816d39761ae733ed55532c0959b7d036d11473d3
dmytroHavliuk <dmytro.havliuk.ki.2022@lpnu.ua>
2024-10-01 19:10:13 +0300

    Update BomberApp.java

--- BomberApp.java
+++ BomberApp.java
@@ -1,14 +1,50 @@
+/**
+ * Пакет лабораторної роботи №3
+ */
 package KI305.Havliuk.Lab3;
 
+import java.io.FileNotFoundException;
+
 /**
- * Клас <code>BomberApp</code> тестує функціональність бомбардувальника.
+ * Клас-драйвер демонструє функціонал класу <code>Bomber</code>
  */
 public class BomberApp {
+	
+	/**
+	 * <code>main</code> є вхідною точкою в програму
+	 * @param args не використовуєьться
+	 */
     public static void main(String[] args) {
-        // Створення бомбардувальника
-        Bomber bomber = new Bomber(2000, 150.0, 40.7, 74.9);
-        
-        // Виконання місії
-        bomber.executeMission();
+        try {
+            // Створення об'єкта Bomber
+            Bomber bomber = new Bomber(800, 5000, "Boeing B-52", 5);
+
+            // Демонстрація основних методів літака
+            bomber.fly();
+            System.out.println("Модель літака: " + bomber.getModel());
+            System.out.println("Швидкість літака: " + bomber.getSpeed() + " км/год");
+            System.out.println("Висота польоту: " + bomber.getAltitude() + " метрів");
+            
+            // Зліт
+            bomber.takeOff();
+
+            // Демонстрація специфічної функції бомбардувальника
+            System.out.println("\nДемонстрація скидання бомб:");
+            bomber.dropBomb();
+            bomber.dropBomb();
+            bomber.dropBomb();
+            bomber.dropBomb();
+            bomber.dropBomb();
+            bomber.dropBomb(); // Спроба скинути бомбу, коли вже немає бомб
+            
+            // Посадка
+            bomber.land();
+
+            // Завершення роботи з ресурсами
+            bomber.dispose();
+            System.out.println("\nРоботу завершено, ресурси звільнено");
+        } catch (FileNotFoundException e) {
+            System.err.println("Помилка при створенні файлу логу: " + e.getMessage());
+        }
     }
 }
